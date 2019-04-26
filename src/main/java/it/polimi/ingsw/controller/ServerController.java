5cd71bc7f65ed3b144e015524efae9fab5e18659
Marco Donadoni <marcodonadoni@live.it>
2019-04-26 14:46:20 +0200

    Better logging

--- src/main/java/it/polimi/ingsw/controller/ServerController.java
+++ src/main/java/it/polimi/ingsw/controller/ServerController.java
@@ -6,6 +6,7 @@ import it.polimi.ingsw.network.View;
 import java.rmi.RemoteException;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ServerController extends LocalServer {
@@ -16,20 +17,20 @@ public class ServerController extends LocalServer {
     @Override
     public synchronized void login(String nickname, View view) throws NicknameAlreadyUsedException {
         if (connectedUser.contains(nickname)) {
-            LOG.warning(() -> "Username already used: "+ nickname);
+            LOG.log(Level.WARNING, "Username already used: {0}", nickname);
             throw new NicknameAlreadyUsedException();
         }
         connectedUser.add(nickname);
-        LOG.info(() -> "New user connected: " + nickname);
+        LOG.log(Level.INFO, "New user connected: {0}", nickname);
         try {
             view.showMessage("Il server ti dice ciao!");
         } catch(RemoteException e) {
-            LOG.severe(() -> "Cannot greet new user " + nickname);
+            LOG.log(Level.SEVERE,"Cannot greet new user {0}", nickname);
         }
         try {
             view.disconnect();
         } catch(RemoteException e) {
-            LOG.severe(() -> "Cannot disconnect user " + nickname);
+            LOG.log(Level.SEVERE, "Cannot disconnect user {0}", nickname);
         }
     }
 }
