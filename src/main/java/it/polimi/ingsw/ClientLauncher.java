fd73233a8b0c1d1cffa8ba88e364fa68d9439d5c
Marco Donadoni <marcodonadoni@live.it>
2019-05-05 17:05:03 +0200

    Partial revert of 5571fe4 to fix RMI

--- src/main/java/it/polimi/ingsw/ClientLauncher.java
+++ src/main/java/it/polimi/ingsw/ClientLauncher.java
@@ -10,7 +10,6 @@ import java.util.logging.Logger;
 public class ClientLauncher {
     public static void main(String[] args) {
         Logger.getLogger("").setLevel(Level.OFF);
-        System.setProperty("java.rmi.server.hostname", "10.0.0.1");
         try {
             LocalView view = new ViewCLI();
             view.run();
